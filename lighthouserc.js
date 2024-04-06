fed2c61a9ad8b665bba470dcd7be70c5de620923
Marcelo Coelho <marcelo.coelho@metaltoad.com>
2024-04-06 10:46:27 -0300

    Acrescenta a sessão assertions no arquivo de configuração do Lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,9 @@ module.exports = {
       url: ['https://www.businessinsider.com/']
     },
     assert: {
+      assertions: {
       'categories:accessibility': ['error', { 'minScore': 0.8 }]
+      }
     },      
     upload: {
       target: 'temporary-public-storage',
